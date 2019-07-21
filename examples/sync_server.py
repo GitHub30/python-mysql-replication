8040b5f084ee61a7d0c4eb3fc1bd87281f003f12
言葉 <funaox@gmail.com>
2019-07-22 04:42:04 +0900

    Update sync_server.py

--- examples/sync_server.py
+++ examples/sync_server.py
@@ -2,64 +2,59 @@ import eventlet
 eventlet.monkey_patch()
 import socketio
 import threading
-import asyncio
+from pycolor import GREEN, END
 
 sio = socketio.Server()
 app = socketio.WSGIApp(sio)
-connected = False
 
 @sio.event
-def connect(sid, environ):
-    print("connect ", sid)
-    global connected
-    connected = True
-
-@sio.event
-def query(sid, sql):
-    print("query ", type(sql))
+def _query(sid, sql):
+    print(GREEN + sql + END)
     conn = pymysql.connect(**MYSQL_SETTINGS)
-    c = conn.cursor()
+    c = conn.cursor(pymysql.cursors.DictCursor)
     c.execute(sql)
-    #conn.close()
-    result = c.fetchall()
-    print('result type', type(result), result)
-    return result
-    #await sio.emit('reply', {'foo': None}, room=sid)
+    conn.close()
+    return c.fetchall()
 
 @sio.event
-def disconnect(sid):
-    print('disconnect ', sid)
+def _enter_room(sid, room_name):
+    sio.enter_room(sid, room_name)
 
 
-import os
+from os import getenv
 import pymysql
 from pymysqlreplication import BinLogStreamReader
+from pymysqlreplication.row_event import WriteRowsEvent, UpdateRowsEvent, DeleteRowsEvent
 
 MYSQL_SETTINGS = {
-    "host": "127.0.0.1",
-    "port": 3306,
-    "user": "root",
-    "passwd": os.getenv("MYSQL_ROOT_PASSWORD", ""),
-    "charset": "utf8mb4",
-    "autocommit": True
+    'host': getenv('MYSQL_HOST', '127.0.0.1'),
+    'port': getenv('MYSQL_PORT', 3306),
+    'user': 'root',
+    'passwd': getenv('MYSQL_ROOT_PASSWORD', ''),
+    'database': getenv('MYSQL_DATABASE'),
+    'charset': 'utf8mb4',
+    'autocommit': True
 }
 
 def binlog():
     # server_id is your slave identifier, it should be unique.
     # set blocking to True if you want to block and wait for the next event at
     # the end of the stream
-    stream = BinLogStreamReader(connection_settings=MYSQL_SETTINGS,
-                                server_id=3,
-                                blocking=True)
+    only_events = [WriteRowsEvent, UpdateRowsEvent, DeleteRowsEvent]
+    stream = BinLogStreamReader(connection_settings=MYSQL_SETTINGS, server_id=3, blocking=True, only_events=only_events)
 
     for binlogevent in stream:
-        print(type(binlogevent), connected)
-        if hasattr(binlogevent, 'rows') and connected:
-            print(sio, type(binlogevent.rows))
-            sio.emit('reply', binlogevent.rows)
-
+        if sio.manager.rooms:
+            values_key = 'after_values' if isinstance(binlogevent, UpdateRowsEvent) else 'values'
+            for row in binlogevent.rows:
+                room = binlogevent.table + '/' + str(row[values_key][binlogevent.primary_key])
+                sio.emit(room, (row, binlogevent.__class__.__name__), room=room)
+            sio.emit(binlogevent.table, (binlogevent.rows, binlogevent.__class__.__name__), room=binlogevent.table)
     stream.close()
+
 
 if __name__ == '__main__':
     threading.Thread(target=binlog).start()
-    eventlet.wsgi.server(eventlet.listen(('', 8080)), app)
+    host = getenv('FIRESQL_HOST', '')
+    port = getenv('FIRESQL_PORT', 8080)
+    eventlet.wsgi.server(eventlet.listen((host, port)), app)
